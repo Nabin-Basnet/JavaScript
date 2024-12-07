0b97000e916d50e09e34d40e473e8758c17f44be
nabin1200 <nabinbasnet1200@gmail.com>
2024-12-07 08:02:18 +0545

    learn-variables-and-constant

---
+++ week 1/2.js
@@ -0,0 +1,18 @@
+// learning about variables and constant
+
+let name = "John"; // Can be reassigned
+const age = 25; // Cannot be reassigned
+var country = "Nepal"; // Older way
+
+
+let text = "Hello"; // string
+let num = 10; // number
+let isTrue = true; // boolean
+let unknown = null; // null
+let notAssigned; // undefined
+
+console.log(text);
+console.log(num);
+console.log(isTrue);
+console.log(unknown);
+console.log(notAssigned);
