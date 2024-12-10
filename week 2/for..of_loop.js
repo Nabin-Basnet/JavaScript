0b9a3eec32fda5217d68709a7702de2b19141665
nabin1200 <nabinbasnet1200@gmail.com>
2024-12-10 15:21:58 +0545

    learn for of loop

---
+++ week 2/for..of_loop.js
@@ -0,0 +1,9 @@
+// The for...of loop in JavaScript is used to iterate over iterable
+//  objects like arrays, strings, maps, sets, and other objects that implement the iterable protocol.
+//   It allows you to loop through the values of the iterable, making it concise and easy to use compared to traditional loops.
+
+let name="my name is nabin basnet";
+
+for(let letter of name){
+    console.log(letter);
+}
