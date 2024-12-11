f513fdd7949c8763d114bf947dab056e8ff36314
nabin1200 <nabinbasnet1200@gmail.com>
2024-12-11 14:51:03 +0545

    leran array method

---
+++ week 2/array_method.js
@@ -0,0 +1,14 @@
+let  fruits=["apple","mango","banana","guava"];
+console.log(fruits);
+
+//push method
+fruits.push("orange","papaya","watermelon");
+console.log(fruits);
+
+//pop method
+console.log(fruits.pop())
+console.log(fruits)
+fruits.pop(apple);
+console.log(fruits);
+
+//their are many types of array method which can be used this is juat a demo of them
