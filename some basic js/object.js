ddee5f899ddaef8c8fb6f9190f351ec97c34712b
nabin1200 <nabinbasnet1200@gmail.com>
2025-01-15 21:06:31 +0545

    object

--- some basic js/object.js
+++ some basic js/object.js
@@ -1,1 +1,14 @@
 // learning about object in detail
+const person={
+    name:"nabin",
+    age:20,
+    address:"sunderharicha",
+    study:"bachlors",
+    contact:{
+        email:"nabinbasnet1200@gmail.com",
+        phone:9815326723
+    }
+}
+person.country="nepal"
+console.log(person.contact.email);
+console.log(person);
