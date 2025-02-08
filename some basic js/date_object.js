1db6cbb7b8c43b8cea671abed4bcef86849e4a42
nabin1200 <nabinbasnet1200@gmail.com>
2025-02-08 08:31:53 +0545

    creating calculator and form handeling

---
+++ some basic js/date_object.js
@@ -0,0 +1,2 @@
+const now = new Date();
+console.log(now); // Outputs the current date and time
--- some basic js/object.js
+++ some basic js/object.js
@@ -1,9 +1,14 @@
 // learning about object in detail
 const person={
-    name:"nabin",
+    first_name:"nabin",
+    middle_name:"jung",
+    last_name:"basnet",
     age:20,
     address:"sunderharicha",
     study:"bachlors",
+    full_name:function(){
+        return this.first_name+" "+this.middle_name+" " +this.last_name;
+    },
     contact:{
         email:"nabinbasnet1200@gmail.com",
         phone:9815326723
@@ -11,4 +16,10 @@ const person={
 }
 person.country="nepal"
 console.log(person.contact.email);
+// delete person.country;
+
 console.log(person);
+console.log(person.full_name());
+for (let x in person){
+    console.log(x)
+}
